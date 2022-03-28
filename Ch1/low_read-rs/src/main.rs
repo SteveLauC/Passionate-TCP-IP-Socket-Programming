ed44bafaa2186ef524b91a6b239c32095e4d0c8b
Steve Lau <stevelauc@outlook.com>
2022-03-28 15:08:57 +0800

    refactor low_read-rs

--- Ch1/low_read-rs/src/main.rs
+++ Ch1/low_read-rs/src/main.rs
@@ -2,29 +2,17 @@ use nix::fcntl::{open, OFlag};
 use nix::sys::stat::Mode;
 use nix::unistd::read;
 
+use std::ffi::CStr;
 use std::os::unix::prelude::RawFd;
 
 fn main() {
     let fd: RawFd =
         open("data.txt", OFlag::O_RDONLY, Mode::empty()).expect("Can not open data.txt");
+
     let mut buf: [u8; 10] = [0_u8; 10];
-    read(fd, &mut buf).expect("Cannot read from data.txt");
 
-
-    // weird way to print this string.
-    for byte in buf {
-        if byte != 0 {
-            print!("{}", char::from(byte));
-        }
-    }
-
-    /*
-        if I use
-
-        ```rsut
-        CStr::from_bytes_with_nul(&buf).expect("Not a valid c string");
-        ```
-        to create the c style string, the program will panic cuz there are more
-        than one NUL in the tail of the array:(
-    */
+    let n_bytes: usize = read(fd, &mut buf).expect("Cannot read from data.txt");
+    let contents: &CStr =
+        CStr::from_bytes_with_nul(&buf[0..=n_bytes]).expect("Not a valid c string");
+    println!("file data: {:?}", contents);
 }
